552644bd89a93ae26640a585f215b7498bc2c758
Elliot Svensen <elliot@elliotsvensen.com>
2019-10-31 22:08:48 +0000

    sorted scrolling issues

--- splashPage.js
+++ splashPage.js
@@ -1,6 +1,11 @@
 var enter = document.querySelector('a.enter');
 var intro = document.querySelector('div.intro');
+var body = document.querySelector('body');
+
 
 enter.addEventListener("click", function(){
   intro.style.display = "none";
+  body.style.overflowY = "auto";
+  document.body.scrollTop = 0; // For Safari
+ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
